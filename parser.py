0e58eb4a2acc9d7d33934708cfe163e223f0e7b9
lbernigaudsamatan <lucas.bernigaudsamatan@agroparistech.fr>
2020-11-10 10:04:29 +0100

    yooooo

--- parser.py
+++ parser.py
@@ -19,18 +19,23 @@ python3 path/to/parser.py path/to/file/to/parse path/to/result/file
 #### imports
 
 import sys
+import pandas as pd
+from io import StringIO
 
 ####
 
-file = open(str(sys.argv[1]),'r')
-bests_hit_file = open(str(sys.argv[2]),'w')
-file = open('Blast_output/Escherichia_coli_536-vs-Escherichia_coli_55989.txt','r')
-bests_hit_file = open('Escherichia_coli_536-vs-Escherichia_coli_55989_compare.txt','w')
+#file = open(str(sys.argv[1]),'r')
+#bests_hit_file = open(str(sys.argv[2]),'w')
+file = open('/Users/Chanco/Desktop/AgroParisTech/3A/AMI2B/Genomique_comparee/full_pipeline/Blast_output/Escherichia_coli_536-vs-Escherichia_coli_55989.txt','r')
+
 txt = file.read()
 querys = txt.split('# BLASTP 2.11.0+')[1:-1]
 database_info = querys[0].split('\n')[1:][1]
-bests_hit_file.write(database_info + '\n')
-bests_hit_file.write('query id\tsubject id\t% identity\talignment length\tmismatches\tgap opens\tq. start\tq. end\ts. start\ts. end\tevalue\tbit score\tquery length\tsubject length\tgaps' + '\n')
+string =''
+string += database_info + '\n'
+string += 'query id\tsubject id\t% identity\talignment length\tmismatches\tgap opens\tq. start\tq. end\ts. start\ts. end\tevalue\tbit score\tquery length\tsubject length\tgaps' + '\n'
+
+
 
 for q in querys:
     lines = q.split('\n')[1:]
@@ -40,9 +45,18 @@ for q in querys:
         continue
     else:
         best_hit = lines[4]
-        bests_hit_file.write(best_hit + '\n')
+        string += best_hit + '\n'
     
 ### Critères de séléction
+
+dt = StringIO(string)
+
+df = pd.read_csv(dt, header = 1,sep="\t")
+df['% couverture'] = (df['alignment length'] / max([df['query length'],df['subject length']]) *100
+ndf = df[df['% identity'] > 70 and df['evalue'] < 0.01 and df['% couverture'] >  60 ]
+
+print(df[df['% identity'] > 70])
+
 
 """
 % identity : 70%
