67a82eb629e0c93fbe30c5eb4bbe0ff0fd0a7bea
Cdubec <71697992+Cdubec@users.noreply.github.com>
2020-11-11 22:01:26 +0100

    Add files via upload

--- BBH_une_boucle_for.py
+++ BBH_une_boucle_for.py
@@ -8,16 +8,25 @@ Created on Tue Nov 10 15:30:11 2020
 import sys
 import pandas as pd
 from io import StringIO
-#pip install progressbar2
-from progressbar import ProgressBar
+
 
 
 #faudra voir comment on séléctionne les deux bons fichiers automatiquement
-name_1 = 'Escherichia_coli_536-vs-Escherichia_coli_55989.csv'
-name_2 = 'Escherichia_coli_55989-vs-Escherichia_coli_536.csv'
-file_1 = pd.read_csv(name_1) #colonne 1 : 536, colonne 2 : 55989
-file_2 = pd.read_csv(name_2) #colonne 1 : 55989, colonne 2 : 536
+#name_1 = 'Escherichia_coli_536-vs-Escherichia_coli_55989.csv'
+#name_2 = 'Escherichia_coli_55989-vs-Escherichia_coli_536.csv'
+#file_1 = pd.read_csv(name_1) #colonne 1 : 536, colonne 2 : 55989
+#file_2 = pd.read_csv(name_2) #colonne 1 : 55989, colonne 2 : 536
 
+#name_1 = str(sys.argv[1])
+name_1 = 'Escherichia_coli_536-vs-Escherichia_coli_55989_compare.csv'
+file_1 = pd.read_csv(name_1)
+name_1 = name_1.split('_compare.csv')[0]
+
+b1 = name_1.split('-vs-')[0]
+b2 = name_1.split('-vs-')[1]
+
+name_2 = b2 + '-vs-' + b1 +'_compare.csv'
+file_2 = pd.read_csv(name_2)
 
 #faudra changer les noms des colonnes en fonction des noms des bactérie, ici 536 puis 55989
 #on est obligé de mettre le même nom de colonne que pour les file sinon on peut pas copier coller les lignes
@@ -41,6 +50,9 @@ for i in range (0,len(file_1.index)):
             if x.loc[x.index[k],'query id'] == file_1.loc[file_1.index[i],'subject id']:
                 #si oui on ajoute cette ligne dan s le fichier contenant les bbh 
                 file_BBH.loc[len(file_BBH.index)] = file_1.loc[i,:].values
+                
+new_name = name_1 + '_BBH.csv'                
+file_BBH.to_csv(new_name, sep='\t', index = False)
 
         
 
